e484a0d9d99c265d0cf87bf4d9afd50c7e6016cb
CF3001TU <CF3001TU@LAPTOP-2M7JBH7T>
2022-04-12 10:27:22 +0530

    First Commit

---
+++ LearningJava/src/Unit01/P1_Task01_MyFirstJavaProject.java
@@ -0,0 +1,12 @@
+package Unit01;
+
+public class P1_Task01_MyFirstJavaProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HEllo WOrld!!\n");
+		System.out.println("This is my first java program");
+
+	}
+
+}
